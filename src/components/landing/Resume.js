7816a6c548da9afb869de4bafe1921bcb703a313
Mix Master Mike <mikeyamato@gmail.com>
2018-04-04 21:16:37 -0700

    updated resume download link

--- src/components/landing/Resume.js
+++ src/components/landing/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
 		// format: //drive.google.com/file/d/FILE_ID/edit?usp=sharing
 		// to be: //drive.google.com/uc?export=download&id=FILE_ID
 		<Popup
-		trigger={<a href='//drive.google.com/uc?export=download&id=1uexZqg5XLhj4Ewp_M7S7pbSyNmypdMJr'>Curriculum vitae</a>}
+		trigger={<a href='//drive.google.com/uc?export=download&id=1teWA4xikErgzR7UYl7hTFvMsGztXIqB2'>Curriculum vitae</a>}
 		content='Worth downloading 😊'
 		inverted
 		position='top center'
