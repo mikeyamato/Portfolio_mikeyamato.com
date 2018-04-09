4be551b5afbc53e5040693ca4cef30878d6ad7a4
Mix Master Mike <mikeyamato@gmail.com>
2018-04-08 17:24:05 -0700

    adjusted header by including my name

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,27 +1,43 @@
 import React, { Component } from 'react';
-import { Container, Menu } from 'semantic-ui-react';
+import { Container, Menu, Segment } from 'semantic-ui-react';
+import Typing from 'react-typing-animation';
 
 export default class Header extends Component {
   
   render() {
 
     return (
-      <Menu attached inverted stackable className='header-edges'>
-        <Container>
-        <Menu.Menu position='right' className='header-font'>
-        <Menu.Item 
-          href='#about'>
-          About
-        </Menu.Item>
-        <Menu.Item href='#portfolio'>
-          Portfolio
-        </Menu.Item>
-        <Menu.Item href='#contact'>
-          Contact
-        </Menu.Item>
-        </Menu.Menu>
-        </Container>
-      </Menu>
+      <Segment inverted attached >
+        <Menu stackable inverted pointing secondary>
+          <Container>
+            <Menu.Item header position='left' className='header-font'>
+              <Typing loop>
+                <Typing.Speed ms={70} />
+                SHINSUKE "MIKE" YAMATO
+                <Typing.Reset count={1} delay={1000} />
+                SOFTWARE ENGINEER
+                <Typing.Reset count={1} delay={1000} />
+                やまと　しんすけ　マイク
+                <Typing.Reset count={1} delay={1000} />
+                ソフトウェア　エンジニア
+                <Typing.Delay ms={1000} /> 
+              </Typing>
+            </Menu.Item>
+            <Menu.Menu position='right' className='header-font'>
+              <Menu.Item 
+                href='#about'>
+                ABOUT
+              </Menu.Item>
+              <Menu.Item href='#portfolio'>
+                PORTFOLIO
+              </Menu.Item>
+              <Menu.Item href='#contact'>
+                CONTACT
+              </Menu.Item>
+            </Menu.Menu>
+          </Container>
+        </Menu>
+      </Segment>
     );
   }
 }
